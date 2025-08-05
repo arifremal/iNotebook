61e8a300f4ab3ee15e87212459f45fcf91be815f
Ariful Islam <remalarif0130@gmail.com>
2025-08-05 23:59:52 +0600

    added

---
+++ backend/middleware/fetchUser.js
@@ -0,0 +1,22 @@
+var jwt = require("jsonwebtoken");
+const JWT_SECRET = "arifisabadboy$$";
+const fetchUser =(req,res,next)=>{
+// get the user from jwt token and add id to request object 
+const token = req.header('auth-token')
+if(!token){
+    res.status(401).send({error:"autheticarte a valid token "})
+}
+try {
+    const data = jwt.verify(token,JWT_SECRET)
+req.user = data.user
+    next()
+    
+} catch (error) {
+     res.status(401).send({error:"autheticarte a valid token "})
+
+    
+}
+
+}
+
+module.exports=fetchUser
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,11 +2,16 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
+var fetchUser = require("../middleware/fetchUser");
 
-// create a user using POST "/api/auth"
+const JWT_SECRET = "arifisabadboy$$";
+
+// Route 1: create a user using POST "/api/auth"
 
 router.post(
-  "/",
+  "/createuser",
   [
     body("name", "Enter a valid name").isLength({ min: 3 }),
     body("email", "Ennnnnter a valid email").isEmail(),
@@ -23,27 +28,91 @@ router.post(
     }
     // check wherer the email with user alewadr exist
     try {
-
       let user = await User.findOne({ email: req.body.email });
-    if (user) {
-      return res.status(400).json({ error: "sorry user email already exist" });
-    }
-    user = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "sorry user email already exist" });
+      }
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+      // create a new user
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: secPass,
+      });
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const auuthToken = jwt.sign(data, JWT_SECRET);
 
-    res.json({ user });
-      
+      res.json({ auuthToken });
     } catch (error) {
-      console.error(error.message)
-      res.status(500).send("some error occured")
-      
+      console.error(error.message);
+      res.status(500).send("some error occured");
     }
-
-    
   }
 );
+
+// Route :2 authenticate use using POST : "/api/login :login not required"
+
+router.post(
+  "/login",
+  [
+    body("email", "Ennnnnter a valid email").isEmail(),
+    body("password", "pas can't be blank").exists(),
+  ],
+  async (req, res) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(400).json({ errors: result.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "please login with corrrect credentials" });
+      }
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res
+          .status(400)
+          .json({ error: "please login with corrrect credentials" });
+      }
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const auuthToken = jwt.sign(data, JWT_SECRET);
+
+      res.json({ auuthToken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internel server error");
+    }
+  }
+);
+
+// Route 3 : get login user details using POST : "/api/getuser login required
+
+router.post("/getuser",fetchUser, async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const user = await User.findById(userId).select("-password");
+    res.send(user)
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internel server error");
+  }
+});
 
 module.exports = router;
